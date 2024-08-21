3aec6112ac89fa884e78fe37790edd6338c4af8c
Angkit Khadka <khadkaankit85@gmail.com>
2024-08-21 02:46:47 -0400

    search bar ui completion

--- src/containers/CreateAlarmScreen.tsx
+++ src/containers/CreateAlarmScreen.tsx
@@ -2,27 +2,25 @@ import {
   View,
   Text,
   Pressable,
-  ScrollView,
   Alert,
   Image,
   TextInput,
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
-import { useNavigation, useRoute } from "@react-navigation/native";
-import { SvgXml } from "react-native-svg";
-import DialogueBox from "../Components/DialogueBox";
-import { Alarm, mapStyle } from "../Constants";
-import MapView from "react-native-maps";
-import { Marker } from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { useState, useEffect } from "react";
 import * as Location from "expo-location";
+import { mapStyle } from "../Constants";
+
+import { useNavigation } from "@react-navigation/native";
 
 import loadingGif from "../assets/icons8-loading.gif";
 import userLocation from "../assets/icons8-user-location-64.png";
 import backButton from "../assets/icons8-back-50.png";
 
 const CreateAlarmScreen = () => {
+  const navigation = useNavigation();
   const [location, setLocation] = useState<Location.LocationObject | null>(
     null
   );
@@ -47,7 +45,6 @@ const CreateAlarmScreen = () => {
   }, []);
 
   if (location === null) {
-    // if (true) {
     return (
       <View
         style={{
@@ -63,7 +60,7 @@ const CreateAlarmScreen = () => {
             width: 60,
             height: 60,
           }}
-        ></Image>
+        />
         <Text
           style={{
             color: "black",
@@ -85,49 +82,64 @@ const CreateAlarmScreen = () => {
   };
 
   return (
-    <View style={{ flex: 1, width: "100%" }}>
-      <MapView
-        customMapStyle={mapStyle}
-        style={{ flex: 1, width: "100%", height: "100%" }}
-        initialRegion={initialRegion}
-        onRegionChange={(region) => {
-          setLocation({
-            coords: {
-              latitude: region.latitude,
-              longitude: region.longitude,
-              altitude: 0,
-              accuracy: 0,
-              altitudeAccuracy: 0,
-              heading: 0,
-              speed: 0,
-            },
-            timestamp: 0,
-          });
-        }}
-      >
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={{ flex: 1, width: "100%" }}>
+        <MapView
+          customMapStyle={mapStyle}
+          style={{ flex: 1, width: "100%", height: "100%" }}
+          initialRegion={initialRegion}
+          onRegionChange={(region) => {
+            setLocation({
+              coords: {
+                latitude: region.latitude,
+                longitude: region.longitude,
+                altitude: 0,
+                accuracy: 0,
+                altitudeAccuracy: 0,
+                heading: 0,
+                speed: 0,
+              },
+              timestamp: 0,
+            });
+          }}
+        >
+          <Marker
+            image={userLocation}
+            draggable
+            coordinate={location.coords}
+            onDragEnd={(e) => {
+              const { latitude, longitude } = e.nativeEvent.coordinate;
+              setLocation({
+                coords: { ...location.coords, latitude, longitude },
+                timestamp: Date.now(),
+              });
+            }}
+          />
+        </MapView>
+
+        {/* Overlaying the back button and input field on top of the MapView */}
         <View
           style={{
-            backgroundColor: "white",
-            width: "90%",
-            height: 60,
-            marginTop: 80,
-            marginHorizontal: "auto",
-            borderRadius: "50%",
-            display: "flex",
-            justifyContent: "space-between",
+            position: "absolute",
+            top: 80,
+            left: 20,
+            right: 20,
+            zIndex: 1,
             flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            backgroundColor: "white",
+            padding: 10,
+            borderRadius: "50%",
           }}
         >
           <Pressable
             style={{
-              width: "20%",
-              height: 50,
               justifyContent: "center",
-              borderColor: "black",
-              borderWidth: 1,
-              display: "flex",
               alignItems: "center",
-              backgroundColor: "red",
+            }}
+            onPress={() => {
+              navigation.goBack();
             }}
           >
             <Image
@@ -135,61 +147,25 @@ const CreateAlarmScreen = () => {
               style={{
                 width: 35,
                 height: 35,
-                justifyContent: "center",
               }}
-            ></Image>
+            />
           </Pressable>
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <View
-                style={{
-                  width: "80%",
-                  height: 50,
-                  justifyContent: "center",
-                  alignItems: "center",
-                  borderColor: "black",
-                }}
-              >
-                <TextInput
-                  placeholder="Enter Alarm Name lkjfdskl kljsdflfsl kjlk flkdslfs jljsdflk jjkldsf lkjdsflklkfjlk flkjflk dsflkllfklkklsd jl"
-                  style={{
-                    width: "100%",
-                    height: 50,
-                    fontSize: 20,
-                    fontFamily: "montserrat",
-                    borderColor: "black",
-                    borderWidth: 1,
-                    paddingLeft: 10,
-                  }}
-                />
-              </View>
-            </View>
-          </TouchableWithoutFeedback>
+
+          <TextInput
+            placeholder="Enter Alarm Name"
+            style={{
+              flex: 1,
+              marginLeft: 10,
+              fontSize: 16,
+              fontFamily: "montserrat",
+              paddingHorizontal: 10,
+              height: 40,
+              borderRadius: 5,
+            }}
+          />
         </View>
-        <Marker
-          style={{
-            width: 50,
-            height: 50,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          image={userLocation}
-          draggable
-          coordinate={location.coords}
-          onDragEnd={(e) =>
-            setLocation(
-              e.nativeEvent.coordinate as unknown as Location.LocationObject
-            )
-          }
-        ></Marker>
-      </MapView>
-    </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
